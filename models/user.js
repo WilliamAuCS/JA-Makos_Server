b52e85f604dfa4c122a9dcc959a5ded746a53d9b
William Au <WilliamAuCS@Gmail.com>
2020-06-08 13:28:37 -0700

    Altered into submodule

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+    email: String, 
+    password: String
+})
+
+// mongoose.model params (name, schemaUsed, db_collectionName)
+module.exports = mongoose.model('user', userSchema, 'users')
